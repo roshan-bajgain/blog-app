7f2a377465f2902b56e4c025c171089f1f4ae42c
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-04-28 15:35:40 +0545

    fix linter error

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -36,20 +36,20 @@ RSpec.describe 'posts', type: :request do
     end
   end
   describe 'GET #post' do
-  before(:each) do
-    get '/users/55/posts/66'
-  end
+    before(:each) do
+      get '/users/55/posts/66'
+    end
 
-  it 'GET requests response status was correct' do
-    expect(response).to have_http_status(:ok)
-  end
+    it 'GET requests response status was correct' do
+      expect(response).to have_http_status(:ok)
+    end
 
-  it 'response body includes correct placeholder text' do
-    expect(response.body).to include('Here is a list of posts for a given user')
-  end
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
 
-  it 'renders a correct template' do
-    expect(response).to render_template(:post)
+    it 'renders a correct template' do
+      expect(response).to render_template(:post)
+    end
   end
-end
 end
