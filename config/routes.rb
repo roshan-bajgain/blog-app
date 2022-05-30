84cc74697adf967a4b7d95101a3013171a36cb91
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-05-30 14:50:47 +0545

    Add show.html.erb as said by previwer and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get "/users/:id/posts", to: "posts#index"
   get "/users/:id", to: "posts#show"
   get "/users/:id/posts/:id", to: "posts#post"
+  get "/users/:user_id/posts/:id", to: "posts#show"
+  get "/users/:id", to: "users#show"
 end
