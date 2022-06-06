edd407d139f91d3342d817ff18868575aad301a8
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-06-06 19:38:41 +0545

    Add proper routes for the pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   get "/users", to: "users#index"
   get "/users/:id/posts", to: "posts#index"
-  get "/users/:id", to: "posts#show"
+  get "/users/:id", to: "users#show"
   get "/users/:id/posts/:id", to: "posts#post"
 end
